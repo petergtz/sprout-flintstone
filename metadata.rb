cd105aea6377da943e548987f4cf28e79f98cdc9
Peter Goetz <pego@de.ibm.com>
2016-05-20 14:32:43 +0200

    Transform sprout-capi into sprout-flintstone

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'capi'
-maintainer       'Cloud Foundry CAPI'
-maintainer_email 'cf-capi-eng@pivotallabs.com'
+name             'flintstone'
+maintainer       'Cloud Foundry Flintstone'
+maintainer_email ''
 license          'unlicense'
-description      'Recipes useful for developing the Cloud Foundry API'
+description      'Recipes useful for developing the Cloud Foundry Bits-Service'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 supports         'mac_os_x'
